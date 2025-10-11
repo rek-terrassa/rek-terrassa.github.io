607b0d7cec3f0d2f7d4d9565919a709f09be5246
rek-terrassa <fcopuerto+rek@gmail.com>
2025-10-11 19:49:31 +0200

    Test upload 4

---
+++ mirrors/sites/277c8835/www.3cat.cat/_resources/config/jitConfig.js
@@ -0,0 +1,19 @@
+window.jitConfig = {
+    resourcesHost: 'https://www.3cat.cat',
+    staticsResourcesHost: 'https://statics.3cat.cat',
+    dinamicsResourcesHost: 'https://dinamics.3cat.cat',
+    version: '2.2510.2',
+    entorn: 'aws_pro',
+    azureAppHost: 'https://registre-usuari.3cat.cat/d3ba85fd-fc05-464a-9540-545c63b2fdf5',
+    authorityDomain: 'registre-usuari.3cat.cat',
+    azureSsoSilentRedirect: 'https://www.3cat.cat/_resources/blank.html',
+    azureRedirect: 'https://www.3cat.cat/3cat/usuaris-redirect/',
+    azureRedirect3catinfo: 'https://www.3cat.cat/3catinfo/usuaris-redirect/',
+    postLogoutRedirectUri: 'https://www.3cat.cat/3cat/',
+    postLogoutRedirectUri3catinfo: 'https://www.3cat.cat/3catinfo/',
+    azureClient: 'c32ae531-ee2c-4df0-8aff-4dfd0b91ba7b',
+    azureHost: 'https://usuarisccma.onmicrosoft.com',
+    apiDELIVERTY: 'https://deliverty2.ccma.cat/deliverty-api',
+    apiDELIVERTYBeta: 'https://deliverty2-beta.ccma.cat/deliverty-api',
+    apiREDLLayouts: 'https://layouts-api.3cat.cat'
+};
